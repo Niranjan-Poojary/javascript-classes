14bd4773a77d4722f3d7e55f5a94eed2547a510d
Niranjan Poojary <121212076+Niranjan-Poojary@users.noreply.github.com>
2024-10-21 00:33:31 +0000

    conversion operation

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,18 @@
+let str1="hello"
+let str2=" niranjan"
+let str=str1+str2;
+console.log(str);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(true);
+console.log("");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+//study prefix and post fix in MDN store 
