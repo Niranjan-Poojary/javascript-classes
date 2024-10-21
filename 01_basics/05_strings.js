3a003440dba853ee96c72076c960cb115d6db1be
Niranjan Poojary <121212076+Niranjan-Poojary@users.noreply.github.com>
2024-10-21 05:39:34 +0000

    string classes done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const gameName = new String('hitesh-hc-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);//gives reverse substring due to negative value
+console.log(anotherString);
+
+const newStringOne = "    niranjan   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+ const url = "https://niranjan.com/niranjan%25poojary";
+ console.log(url.replace('%25','-'));
+
+ console.log(url.includes('sunder'));
+
+ console.log(gameName.split('-'));
+ console.log(gameName.anchor("niranjan"));
---
+++ memory/js-memory.js
@@ -0,0 +1,3 @@
+//stack memory and Heap memeory
+//Stack memory used in Primitive datatype
+//Heap memory used in non primitive data type
