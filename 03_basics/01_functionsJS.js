d959feb75072c5e57573fe56afcb884deb5041e1
Niranjan Poojary <121212076+Niranjan-Poojary@users.noreply.github.com>
2024-10-23 07:28:50 +0000

    functions with objects and array in JS

--- 03_basics/01_functionsJS.js
+++ 03_basics/01_functionsJS.js
@@ -24,4 +24,34 @@
     }
     return `${username} just logged in`
  }
- console.log(loginUserMessage("niranjan"));
+ //console.log(loginUserMessage("niranjan"));
+
+
+ function calculateCartPrice(...num1){
+     return num1;
+ }
+ //console.log(calculateCartPrice(200,300,400));
+
+ const user = {
+   username:"hitesh",
+   price:199
+ }
+ function handleObject(anyobject){
+   console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+ }
+//handleObject(user)
+
+//direct pass the object
+handleObject({
+   username:"sam",
+   price:399
+})
+
+//pass the array
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+   return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,1000]));
