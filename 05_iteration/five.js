e853c83644a24f4294cb6d350cfccf5d87bae27f
Niranjan Poojary <121212076+Niranjan-Poojary@users.noreply.github.com>
2024-10-25 04:54:14 +0000

    High Order Array loops in JS

---
+++ 05_iteration/five.js
@@ -0,0 +1,45 @@
+const coding = ["js","ruby","java","c++","python"]
+
+// coding.forEach( function (item){
+//     console.log(item);
+    
+// })
+
+// coding.forEach((item)=>{
+//   console.log(item);
+// } )
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+
+coding.forEach((item,index,arr)=>{
+    //console.log(item, index, arr);
+})
+
+const myCoding = [
+    {
+        languageName:"javascipt",
+        languageFileName:"js"
+    },
+    {
+        languageName:"java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+]
+
+myCoding.forEach( (item) =>{
+    console.log(item.languageFileName)
+})
+
+
+
+
+
